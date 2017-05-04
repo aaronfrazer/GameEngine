e4471e7d502d86a40851a0fb34f2480d51baa3bb
aaronfrazer <aarfrazer@gmail.com>
2017-05-03 21:58:56 -0400

    Fixed 3D model rotation.
    Changed calculation in Maths.java

--- src/toolbox/Maths.java
+++ src/toolbox/Maths.java
@@ -26,11 +26,10 @@ public class Maths
 		Matrix4f matrix = new Matrix4f();
 		matrix.setIdentity();
 		Matrix4f.translate(translation, matrix, matrix);
-		Matrix4f.rotate((float)Math.toRadians(rx), new Vector3f(1, 0, 0), matrix, matrix);
-		Matrix4f.rotate((float)Math.toRadians(rx), new Vector3f(0, 1, 0), matrix, matrix);
-		Matrix4f.rotate((float)Math.toRadians(rx), new Vector3f(0, 0, 1), matrix, matrix);
-		Matrix4f.scale(new Vector3f(scale, scale, scale), matrix, matrix);
-		
+		Matrix4f.rotate((float) Math.toRadians(rx), new Vector3f(1,0,0), matrix, matrix);
+		Matrix4f.rotate((float) Math.toRadians(ry), new Vector3f(0,1,0), matrix, matrix);
+		Matrix4f.rotate((float) Math.toRadians(rz), new Vector3f(0,0,1), matrix, matrix);
+		Matrix4f.scale(new Vector3f(scale,scale,scale), matrix, matrix);
 		return matrix;
 	}
 	
@@ -41,16 +40,13 @@ public class Maths
 	 */
 	public static Matrix4f createViewMatrix(Camera camera)
 	{
-		  Matrix4f viewMatrix = new Matrix4f();
-		  viewMatrix.setIdentity();
-		  Matrix4f.rotate((float) Math.toRadians(camera.getPitch()), new Vector3f(1,0,0), viewMatrix,
-		    viewMatrix);
-		  Matrix4f.rotate((float) Math.toRadians(camera.getYaw()), new Vector3f(0,1,0), viewMatrix,
-		    viewMatrix);
-		  Vector3f cameraPos = camera.getPosition();
-		  Vector3f negativeCameraPos = new Vector3f(-cameraPos.x,-cameraPos.y,-cameraPos.z);
-		  Matrix4f.translate(negativeCameraPos, viewMatrix, viewMatrix);
-		  
-		  return viewMatrix;
+		Matrix4f viewMatrix = new Matrix4f();
+		viewMatrix.setIdentity();
+		Matrix4f.rotate((float) Math.toRadians(camera.getPitch()), new Vector3f(1, 0, 0), viewMatrix, viewMatrix);
+		Matrix4f.rotate((float) Math.toRadians(camera.getYaw()), new Vector3f(0, 1, 0), viewMatrix, viewMatrix);
+		Vector3f cameraPos = camera.getPosition();
+		Vector3f negativeCameraPos = new Vector3f(-cameraPos.x,-cameraPos.y,-cameraPos.z);
+		Matrix4f.translate(negativeCameraPos, viewMatrix, viewMatrix);
+		return viewMatrix;
 	}
 }
